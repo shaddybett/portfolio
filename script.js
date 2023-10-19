e9f4eaa3660bffa9418be0dad8b4aafa3c0b84f3
Shaddy <shadybett540@gmail.com>
2023-10-19 13:26:34 +0300

    did some revisions on functions

--- script.js
+++ script.js
@@ -1,12 +1,57 @@
-const radii = [2,5,7,6]
-
-function calculateCircumference(){
-    for(let i = 0;i<radii.length;i++){
-        const circleCircumference = radii[i]*2 * Math.PI;
-        console.log(`the radii is ${radii[i]} and the circumference is ${circleCircumference}`)
-    }
-}calculateCircumference();
+const radii = [2,3,5,6]
 
 function calculateArea(){
-    
-}
+    for(let i = 0;i<radii.length;i++){
+        const area = radii[i] * radii[i] * Math.PI
+        console.log(`my radius is ${radii[i]} and my area is ${area}`)
+    }
+}calculateArea();
+
+
+let names = ['jane','alice','blue']
+
+function greet(){
+    for(let i = 0;i<names.length;i++){
+      const namesB= names[i]
+      console.log(`hello ${namesB}`)
+        
+    }
+}greet();
+
+let people = ['tom','hassan','jerry']
+
+function payPeople(){
+    for(let i = 0;i<people.length;i++){
+        const paying = people[i]
+        console.log(`pay ${paying}`)
+    }
+    setTimeout(function(){
+        console.log('paid')
+    },10000)
+}payPeople();
+
+
+const numbers = [9,8,7,6,5]
+function displayNumbers(){
+    const numbers = [1,5,6,7,9]
+    console.log(`the flow ${numbers}`)
+}displayNumbers();
+
+// console.log(`the flow ${numbers}`)
+
+const pets = ['dog','cat','hen']
+
+function displayPets(){
+    const pets = ['sheep','goat']
+    console.log(`My ${pets}`)
+}displayPets();
+// console.log(`My ${pets}`)
+ function greet (){
+const name = ['alice']
+    console.log(`hello ${name}`)
+ setTimeout(function(){
+    console.log('timer')
+ },5000)
+ }greet()
+
+ 
